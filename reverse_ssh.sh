64848f2fab7b88efafd0171597de9b1c762bc691
vbipin <vbipin@yahoo.com>
2018-05-31 18:27:42 +0800

    added -p option

--- reverse_ssh.sh
+++ reverse_ssh.sh
@@ -11,7 +11,8 @@ ip=`cat bipin_laptop | perl -ne 'if(/([0-9\.]+)/){print $1;}'`
 #ssh -o "ExitOnForwardFailure yes" -o "StrictHostKeyChecking no" -N -R 33033:localhost:22 ssh_user@$ip
 
 #here we use another server as remote
-ssh -o "ExitOnForwardFailure yes" -o "StrictHostKeyChecking no" -N -R 33033:10.0.16.6:22 ssh_user@$ip
+#assume home port is 22022
+ssh -p 22022 -o "ExitOnForwardFailure yes" -o "StrictHostKeyChecking no" -N -R 33033:10.0.16.6:22 ssh_user@$ip
 
 
 #Asimple way to run this is
